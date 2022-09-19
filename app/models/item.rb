554aa41e86a4a62f5691899778b471df0a574239
masa0501 <nakamura19980501@gmail.com>
2022-09-19 10:56:22 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :area
+  belongs_to :days
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :days_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
