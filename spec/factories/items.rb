e6d09dee4f64b33a1a4d2b1e8b8b36eeee33e510
masa0501 <nakamura19980501@gmail.com>
2022-09-20 18:12:30 +0900

    Rubocopを実行

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,7 +9,7 @@ FactoryBot.define do
     area_id            { 2 }
     days_id            { 2 }
 
-    association :user 
+    association :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
