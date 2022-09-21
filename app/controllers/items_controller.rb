78b36659bc8b11f8d9a8124c63d9f3ec77a53724
masa0501 <nakamura19980501@gmail.com>
2022-09-21 16:46:41 +0900

    商品情報編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit]
+  before_action :set_item,      only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,17 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path
     else
       render :edit
     end
@@ -46,5 +44,9 @@ class ItemsController < ApplicationController
   def move_to_index
     @item = Item.find(params[:id])
     redirect_to action: :index unless @item.user == current_user
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
