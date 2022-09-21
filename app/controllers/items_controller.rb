ccabce4b9cfa8e9a29762eded6e1a6c06c621995
masa0501 <nakamura19980501@gmail.com>
2022-09-21 16:02:13 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index,     only: [:edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -36,20 +36,15 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :price, :category_id, :condition_id, :delivery_charge_id, :area_id,
-                               :days_id).merge(user_id: current_user.id)
+                                 :days_id).merge(user_id: current_user.id)
   end
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless @item.user == current_user
-      redirect_to action: :index
-    end
+    redirect_to action: :index unless @item.user == current_user
   end
-
 end
